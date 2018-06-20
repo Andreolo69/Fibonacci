d5fbfb460afcf13e9f26b27434acf97d73b18778
Andrea <andreo2000_2000@yahoo.it>
2018-06-20 11:02:51 +0200

    Tidying up

---
+++ src/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Inserta numero: ");
+        int num=sc.nextInt();
+        int i=1;
+        int sum=0;
+        for( i=1; i<num; i++){
+            i=i+1;
+            sum=i
+
+        }
+
+    }
+}
