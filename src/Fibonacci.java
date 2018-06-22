f977c7b144c98909468a77bf9efccaa175778c88
Andrea <andreo2000_2000@yahoo.it>
2018-06-22 11:02:46 +0200

    nada

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -8,13 +8,21 @@ public class Fibonacci {
 
         System.out.print("Inserta numero: ");
         int num=sc.nextInt();
-        int i=1;
-        int sum=0;
-        for( i=1; i<num; i++){
-            i=i+1;
-            sum=i
+     int i;
+int sum=1;
+int a=1;
+     for(i=1; i<num; i++) {   //i++=1, 2, 3, 4, 5, 6.....
+                                    //a+i=sum //a+i=sum
+     //1,1,2,3,5,8,13...1+1=2, 1+2=3, 2+3=5, 5+3=8, 5+8=13, 8+13=21;
+         i=sum;
 
-        }
+         sum=a+i;
 
+         a=i;//
+
+         System.out.print(sum+" ");
+
+
+     }
     }
 }
